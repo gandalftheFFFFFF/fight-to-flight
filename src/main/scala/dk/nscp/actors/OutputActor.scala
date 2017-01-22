a85545bcbb6612de30efd86e9bd33163808a43e3
Niels Pedersen <nielsschroderpedersen@gmail.com>
2017-01-22 16:42:57 +0100

    Add output file management

--- src/main/scala/dk/nscp/actors/OutputActor.scala
+++ src/main/scala/dk/nscp/actors/OutputActor.scala
@@ -4,14 +4,40 @@ import akka.actor.Actor
 import dk.nscp.actors.ActorObjects._
 import java.text.SimpleDateFormat
 import java.io.FileWriter
+import java.io.File
+import com.typesafe.config.ConfigFactory
 
 class OutputActor extends Actor {
+
+  val conf = ConfigFactory.load()
+
+  val outputDir = "output"
+
+  def nextFileNumber: Int = {
+    val regex = """(output\d*.csv)""".r
+    ((new File(outputDir))
+      .listFiles
+      .map(_.getName)
+      .filter(file => regex.findFirstIn(file).isDefined)
+      .length)
+  }
+
   def receive = {
     case record: Record => 
       // print to output file
       val fw = new FileWriter("output/output.csv", true)
       fw.write(record.csvFormat + "\n")
       fw.close()
+      checkFiles
   }
+
+  def checkFiles: Unit = {
+    val oldFile = new File(outputDir + "/output.csv")
+    if (oldFile.length > conf.getInt("max-filesize-bytes")) {
+      val asArchiveFile = oldFile.renameTo(new File(s"$outputDir/output$nextFileNumber.csv"))
+    }
+  }
+
+
 
 }
